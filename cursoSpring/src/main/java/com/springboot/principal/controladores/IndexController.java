bf1d20118faf99a800f41b107f14e14f983555fb
Juan Antonio Rosas <juan.antoniorr@hotmail.com>
2018-02-21 18:05:31 -0600

    ultimos cambios basicos

--- cursoSpring/src/main/java/com/springboot/principal/controladores/IndexController.java
+++ cursoSpring/src/main/java/com/springboot/principal/controladores/IndexController.java
@@ -1,16 +1,20 @@
 package com.springboot.principal.controladores;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+	@Value("${controller.bienvenida}")
+	private String bienvenida;
+
 	
 	
 	@GetMapping ("/index")
 	public String index (Model model){
-		model.addAttribute("titulo", "Hola mundo Spring");
+		model.addAttribute("titulo", bienvenida);
 		return "index";
 		
 	}
