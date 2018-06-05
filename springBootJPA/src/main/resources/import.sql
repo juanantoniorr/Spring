b44a9666021ea0f9cdb291a960caa0998859cacc
Juan Antonio Rosas <juan.antoniorr@hotmail.com>
2018-06-05 13:26:49 -0500

    Proyecto profesor Proyecto descargado del profesor para el video 48

--- springBootJPA/src/main/resources/import.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO clientes (id, nombre, apellido, email, creacion) VALUES(1, 'Andres', 'Guzman', 'profesor@bolsadeideas.com', '2017-08-28');
-INSERT INTO clientes (id, nombre, apellido, email, creacion) VALUES(2, 'John', 'Doe', 'john.doe@gmail.com', '2017-08-28');
