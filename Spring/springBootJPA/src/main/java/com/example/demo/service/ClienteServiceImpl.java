a73d02fd8ec606143af033de72d8c3484fed35b0
Juan <juan.antoniorr@hotmail.com>
2018-06-09 17:45:34 -0500

    Upload archivos Aqui se suben archivos al directorio upload del proyecto pero aun no al servidor

---
+++ Spring/springBootJPA/src/main/java/com/example/demo/service/ClienteServiceImpl.java
@@ -0,0 +1,54 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.entity.Cliente;
+import com.example.demo.entity.dao.IClienteDao;
+
+//Con esta clase puedo usar diferentes dao
+@Service
+public class ClienteServiceImpl implements IClienteService {
+	@Autowired
+	IClienteDao clienteDao;
+	@Transactional(readOnly=true)
+	@Override
+	public List<Cliente> findAll() {
+		
+		return (List<Cliente>) clienteDao.findAll();
+	}
+
+	@Transactional
+	@Override
+	public void save(Cliente cliente) {
+	
+		clienteDao.save(cliente);
+	}
+
+	@Transactional(readOnly=true)
+	@Override
+	public Cliente findOne(long id) {
+		
+		return clienteDao.findOne(id);
+	}
+
+	@Transactional
+	@Override
+	public void eliminar(Long id) {
+		clienteDao.delete(id);
+		
+	}
+
+	@Override
+	public Page<Cliente> findAll(Pageable pageable) {
+		
+		return clienteDao.findAll(pageable);
+	}
+	
+
+}
