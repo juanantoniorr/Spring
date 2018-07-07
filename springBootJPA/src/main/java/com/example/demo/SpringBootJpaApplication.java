5c004986c93797b0f126a24088bc08374053f933
Juan <juan.antoniorr@hotmail.com>
2018-07-07 16:53:26 -0500

    Crea directorio uploads en automatico

--- springBootJPA/src/main/java/com/example/demo/SpringBootJpaApplication.java
+++ springBootJPA/src/main/java/com/example/demo/SpringBootJpaApplication.java
@@ -1,12 +1,29 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.demo.service.IUploadFileService;
+
 @SpringBootApplication
-public class SpringBootJpaApplication {
+public class SpringBootJpaApplication implements CommandLineRunner { //interfaz que actua en cuanto se ejecuta la app
+	//Inyecto la interfaz para crear y eliminar el directorio
+	@Autowired
+	IUploadFileService uploadFileService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJpaApplication.class, args);
+	}
+	//Es bueno utilizar esta anotacion para hacer el codigo mas legible y que te avise si es que estas usando mal
+	//el metodo 
+	@Override
+	public void run(String... arg0) throws Exception {
+		//Elimina el directorio uploads si existe y despues lo crea todo al iniciar la app
+		uploadFileService.deleteAll();
+		uploadFileService.init();
+		
+		
 	}
 }
--- springBootJPA/src/main/java/com/example/demo/service/IUploadFileService.java
+++ springBootJPA/src/main/java/com/example/demo/service/IUploadFileService.java
@@ -1,5 +1,7 @@
 package com.example.demo.service;
 
+import java.io.IOException;
+
 import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -10,4 +12,8 @@ public Resource load (String fileName);
 public String copy (MultipartFile file);
 //Para saber si lo borro o no 
 public boolean delete (String filename);
+//borra todo el directorio uploads
+public void deleteAll();
+//Crea el directorio uploads
+public void init() throws IOException;
 }
